2639514c75c370c8d1a0cbb315022df379a242bc
huonglee23 <huonglee160923@gmail.com>
2024-09-21 10:45:37 +0700

    BaseProject

---
+++ HotelBookingSystem/Repositories/Interfaces/IRoomRepository.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Models;
+
+namespace HotelBookingSystem.Repositories.Interfaces
+{
+    public interface IRoomRepository
+    {
+        IEnumerable<Room> GetAll();
+
+        void Add(Room room);
+
+        void Edit(Room room);
+
+        void Remove(int roomId);
+
+        void Save();
+    }
+}
