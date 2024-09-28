5bb43764cadf8a0488df5f48aeffde8844349a5b
huonglee23 <huonglee160923@gmail.com>
2024-09-28 22:26:12 +0700

    demo

--- HotelBookingSystem/obj/Debug/net6.0/HotelBookingSystem.AssemblyInfo.cs
+++ HotelBookingSystem/obj/Debug/net6.0/HotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d586ffeea06f0626e3c81b5a5f21a9e8ae5b817")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
